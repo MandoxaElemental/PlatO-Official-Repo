1b0f4ca35501d46a28877c6f10055f5a1b221be2
Gianpaolo Reinares <gprcards@gmail.com>
2025-04-08 11:30:22 -0700

    Added the Premium Page

---
+++ src/app/(pages)/Premium/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Premium = () => {
+  return (
+    <div className='pt-10 px-5 w-full'>
+      <div className='border-b-1 border-solid border-slate-300 p-2 text-2xl font-semibold text-center'>
+            Premium
+        </div>
+        <div>Give Me Money...</div>
+    </div>
+  )
+}
+
+export default Premium
