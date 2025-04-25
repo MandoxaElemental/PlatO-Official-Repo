1b77ab68c5154a261165512cbd8fcc0574a7b973
Gianpaolo Reinares <gprcards@gmail.com>
2025-04-25 15:04:38 -0700

    Making all of the buttons consistant

--- src/app/Components/Recommended.tsx
+++ src/app/Components/Recommended.tsx
@@ -13,19 +13,19 @@ const Recommended = () => {
             <Image width={50} height={50} className='w-[120px] h-[120px] object-cover object-center rounded-full p-2' src="../assets/person.svg" alt="profile" />
             <p className='font-semibold'>UserName</p>
             <p>[Title]</p>
-            <Button className="rounded-full m-4">Follow</Button>
+            <Button className="m-4 rounded-full h-8 bg-blue-200 hover:bg-blue-400 text-black cursor-pointer dark:bg-blue-100 dark:hover:bg-blue-200">Follow</Button>
         </div>
         <div className='border-1 border-slate-300 border-solid rounded-xl flex flex-col items-center'>
             <Image width={50} height={50} className='w-[120px] h-[120px] object-cover object-center rounded-full p-2' src="../assets/person.svg" alt="profile" />
             <p className='font-semibold'>UserName</p>
             <p>[Title]</p>
-            <Button className="rounded-full m-4">Follow</Button>
+            <Button className="m-4 rounded-full h-8 bg-blue-200 hover:bg-blue-400 text-black cursor-pointer dark:bg-blue-100 dark:hover:bg-blue-200">Follow</Button>
         </div>
         <div className='border-1 border-slate-300 border-solid rounded-xl flex flex-col items-center'>
             <Image width={50} height={50} className='w-[120px] h-[120px] object-cover object-center rounded-full p-2' src="../assets/person.svg" alt="profile" />
             <p className='font-semibold'>UserName</p>
             <p>[Title]</p>
-            <Button className="rounded-full m-4">Follow</Button>
+            <Button className="m-4 rounded-full h-8 bg-blue-200 hover:bg-blue-400 text-black cursor-pointer dark:bg-blue-100 dark:hover:bg-blue-200">Follow</Button>
         </div>
         </div>
     </div>
